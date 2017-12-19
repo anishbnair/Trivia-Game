032bcad5bc50ed31326a34b0f3011f939d38202a
anishbnair <nbanish@gmail.com>
2017-12-18 22:23:43 -0500

    added an object to store trivia questions, answer options and correct anser

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// VARIABLES/DATA
+// ==================================================================================================================================
+// Object to store question, answer options and correct answer 
+var triviaQuestions = {
+    q1: {
+        question: "Who played Lord Voldemort in the movies?",
+        answers: ["JEREMY IRONS", "TOM HIDDLESTON", "GARY OLDMAN", "RALPH FIENES"],
+        correctAnswer: "RALPH FIENES",
+    },
+    q2: {
+        question: "Who played Ron Weasley in the movies?",
+        answers: ["KIERAN CULKIN", "RUPERT GRINT", "TYLER HOECHLIN", "DANIEL RADCLIFFE"],
+        correctAnswer: "RUPERT GRINT",
+    },
+}
+
