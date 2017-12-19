64711564153799f4eb5bc17839dc7e787071f972
anishbnair <nbanish@gmail.com>
2017-12-19 10:17:06 -0500

    included a function to activate one active section at a time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,28 @@
+// Pseudo code 
+//===============================================================================================================================
+
+// Click Events
+
+// Click event for Start button
+// Click event when user choose an answer option
+// Click event when user selects 'Start Over' button at the end of the game 
+
+// Functions 
+
+// 1. Function to display one active section at a time
+//  hide all sections 
+//  activate only the section which is passed to the function 
+
+// Function to display question and answers
+// Function to start timer
+// Function to stop timer
+// Function to reset timer 
+// Function to validate answer selected
+//=================================================================================================================================
+
 // VARIABLES/DATA
 // ==================================================================================================================================
-// Object to store question, answer options and correct answer 
+// Object to store questions, answer options and correct answer 
 var triviaQuestions = {
     q1: {
         question: "Who played Lord Voldemort in the movies?",
@@ -14,3 +36,25 @@ var triviaQuestions = {
     },
 }
 
+var section = ""; 
+var activateStart = $("#startSection");
+var activateQuestion = $("#questionSection");
+var activateAnswers = $("#answerSection");
+var activateResults = $("#resultsSection");
+
+
+// Functions
+//==================================================================================================================================
+// Function to display only one active section at a time
+function activeSection(section) {
+    //hide all sections 
+    activateStart.hide();
+    activateQuestion.hide();
+    activateAnswers.hide();
+    activateResults.hide(); 
+    // activate only one section
+    if (section) {
+        section.show(); 
+    }
+}
+//activeSection(activateStart)
